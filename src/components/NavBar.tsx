97bada21a9c327a13d3af2dc4f52eee76314c4fb
aman-senpai <jemscollin1@gmail.com>
2023-10-27 22:31:09 +0530

    Build the Color Mode Switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,21 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
     return (
-        <HStack>
-            <Image
-                src={logo}
-                boxSize="50px"
-            />
-            <Text>GamezHub</Text>
+        <HStack
+            justifyContent="space-between"
+            padding="10px"
+        >
+            <HStack>
+                <Image
+                    src={logo}
+                    boxSize="50px"
+                />
+                <Text>GamezHub</Text>
+            </HStack>
+            <ColorModeSwitch />
         </HStack>
     );
 };
