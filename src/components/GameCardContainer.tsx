1d882ad31670853002f5382a9365b56643ffb3f5
aman-senpai <jemscollin1@gmail.com>
2023-10-28 22:06:48 +0530

    Display Genre

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -8,7 +8,7 @@ interface Props {
 const GameCardContainer = ({ children }: Props) => {
     return (
         <Box
-            width="220px"
+            width="100%"
             borderRadius={10}
             overflow={"hidden"}
         >
