e0ec396ca00c658d0a6ddd955c315cf9f81ee49d
aman-senpai <jemscollin1@gmail.com>
2023-10-29 13:23:31 +0530

    Customize the theme to get better grays

--- src/theme.ts
+++ src/theme.ts
@@ -5,6 +5,22 @@ const config: ThemeConfig = {
     useSystemColorMode: false,
 }
 
-const theme = extendTheme({ config })
+const theme = extendTheme({
+    config,
+    colors: {
+        gray: {
+            50: '#f9f9f9',
+            100: '#f3f3f3',
+            200: '#e5e5e5',
+            300: '#d9d9d9',
+            400: '#c4c4c4',
+            500: '#a3a3a3',
+            600: '#737373',
+            700: '#525252',
+            800: '#262626',
+            900: '#171717',
+        }
+    }
+})
 
 export default theme
