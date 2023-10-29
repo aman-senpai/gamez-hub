6d18d6984c7dfe822121ec5a851a6c7c088920ba
aman-senpai <jemscollin1@gmail.com>
2023-10-29 11:42:47 +0530

    Build search Input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,33 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
     return (
         <HStack
             justifyContent="space-between"
             padding="10px"
+            marginLeft={3}
         >
-            <HStack>
+            <Flex
+                marginRight={10}
+                paddingTop={1}
+            >
                 <Image
                     src={logo}
                     boxSize="50px"
                 />
-                <Text>GamezHub</Text>
-            </HStack>
+                <Text
+                    fontSize="2xl"
+                    paddingTop={1}
+                    fontFamily="monospace"
+                    fontWeight="bold"
+                >
+                    GamezHub
+                </Text>
+            </Flex>
+            <SearchInput />
             <ColorModeSwitch />
         </HStack>
     );
