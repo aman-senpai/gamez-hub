02f901e88f8bc185120698d46cb442a7d85afd24
aman-senpai <jemscollin1@gmail.com>
2023-11-03 22:36:48 +0530

    Implement infinite queries

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,11 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
 
 const apiClient = new APIClient<Game>("/games")
-
-// TODO: Fix this duplication
 
 export interface Game {
     id: number;
@@ -17,10 +15,12 @@ export interface Game {
 }
 
 
+
+
 const useGames = (gameQuery: GameQuery) =>
-    useQuery<FetchResponse<Game>, Error>({
+    useInfiniteQuery<FetchResponse<Game>, Error>({
         queryKey: ["games", gameQuery],
-        queryFn: () =>
+        queryFn: ({ pageParam = 1 }) =>
             apiClient
                 .getAll({
                     params:
@@ -28,9 +28,13 @@ const useGames = (gameQuery: GameQuery) =>
                         genres: gameQuery.genre?.id,
                         parent_platforms: gameQuery.platform?.id,
                         ordering: gameQuery.sortOrder,
-                        search: gameQuery.searchText
+                        search: gameQuery.searchText,
+                        page: pageParam
                     }
-                })
+                }),
+        getNextPageParam: (lastPage, allPages) => {
+            return lastPage.next ? allPages.length + 1 : undefined
+        }
 
     });
 
