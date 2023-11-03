f7e0db91dc672328ee26173401afb709686a0808
aman-senpai <jemscollin1@gmail.com>
2023-11-03 22:36:48 +0530

    Implement infinite queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
+import { Box, Button, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
