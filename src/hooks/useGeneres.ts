2c96ce24081eaf73e562a9d1e237f2552b0bf149
aman-senpai <jemscollin1@gmail.com>
2023-11-03 21:05:18 +0530

    Use react-query for genres list optimization.

--- src/hooks/useGeneres.ts
+++ src/hooks/useGeneres.ts
@@ -1,4 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
+
+
 
 export interface Genre {
     id: number;
@@ -6,5 +11,12 @@ export interface Genre {
     image_background: string;
 }
 
-const useGeneres = () => ({ data: genres, isLoading: false, error: null })
+
+const useGeneres = () => useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+        apiClient.get<FetchResponse<Genre>>("/genres").then(res => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours,
+    initialData: { count: genres.length, results: genres }
+})
 export default useGeneres;
