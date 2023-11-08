074fe62bd4b0fa214ae5520c194c262e1d5d59d8
aman-senpai <jemscollin1@gmail.com>
2023-11-08 14:40:42 +0530

    Refactor: Move entity interfaces

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -7,7 +7,7 @@ import {
     Skeleton,
     SkeletonText,
 } from "@chakra-ui/react";
-import { Game } from "../hooks/useGames";
+import { Game } from "../entities/Game";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
