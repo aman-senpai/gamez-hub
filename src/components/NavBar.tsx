125eff1936ccefea5fc084b68092a991f2493803
aman-senpai <jemscollin1@gmail.com>
2023-11-08 16:37:40 +0530

    Fix the NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import { Flex, HStack, Image, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
@@ -10,23 +11,25 @@ const NavBar = () => {
             padding="10px"
             marginLeft={3}
         >
-            <Flex
-                marginRight={10}
-                paddingTop={1}
-            >
-                <Image
-                    src={logo}
-                    boxSize="50px"
-                />
-                <Text
-                    fontSize="2xl"
+            <Link to="/">
+                <Flex
+                    marginRight={10}
                     paddingTop={1}
-                    fontFamily="monospace"
-                    fontWeight="bold"
                 >
-                    GamezHub
-                </Text>
-            </Flex>
+                    <Image
+                        src={logo}
+                        boxSize="50px"
+                    />
+                    <Text
+                        fontSize="2xl"
+                        paddingTop={1}
+                        fontFamily="monospace"
+                        fontWeight="bold"
+                    >
+                        GamezHub
+                    </Text>
+                </Flex>
+            </Link>
             <SearchInput />
             <ColorModeSwitch />
         </HStack>
